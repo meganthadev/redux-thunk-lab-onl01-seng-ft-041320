e4dc4c27a4611a31b56d16a95ad79b67dfd2f798
Megan Horsfield <meganthadev@gmail.com>
2020-09-03 02:57:58 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        {this.handleLoading()}
       </div>
     );
   }
